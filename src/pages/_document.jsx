9e0c3168069ed9e0e90187b59cebd2021d2c0b0a
BowTied Fullstack <me@bowtiedstack.com>
2023-09-27 11:17:03 -0400

    Fix build

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,4 @@
 import { Head, Html, Main, NextScript } from 'next/document'
-import { Script } from 'next/script'
 
 export default function Document(props) {
   let pageProps = props.__NEXT_DATA__?.props?.pageProps
@@ -23,12 +22,12 @@ export default function Document(props) {
       </Head>
       <body className="flex h-full flex-col">
         {/* <!-- Google tag (gtag.js) --> */}
-        <Script
+        <NextScript
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-CSLPL8X5FZ"
         />
-        <Script async src="/gtag.js" />
-        <Script id="google-analytics">
+        <NextScript async src="/gtag.js" />
+        <NextScript id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -36,7 +35,7 @@ export default function Document(props) {
 
             gtag('config', 'G-CSLPL8X5FZ');
           `}
-        </Script>
+        </NextScript>
         <Main />
         <NextScript />
       </body>
