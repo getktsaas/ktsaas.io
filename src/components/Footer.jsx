eeefd60b732858a0203579f34fcfe5c199b16bbc
BowTied Fullstack <me@bowtiedstack.com>
2023-09-26 22:51:09 -0400

    Fix docs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,9 +14,10 @@ export function Footer() {
           </div>
           <nav className="mt-10 text-sm" aria-label="quick links">
             <div className="-my-1 flex justify-center gap-x-6">
-              <NavLink href="#features">Features</NavLink>
-              <NavLink href="#testimonials">Testimonials</NavLink>
-              <NavLink href="#pricing">Pricing</NavLink>
+              <NavLink href="/#features">Features</NavLink>
+              <NavLink href="/#testimonials">Testimonials</NavLink>
+              <NavLink href="/#pricing">Pricing</NavLink>
+              <NavLink href="/docs/">Docs</NavLink>
             </div>
           </nav>
         </div>
